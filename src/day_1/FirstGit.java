3adb9a3b4ab01baa112205ea7996c0a33da60ca0
Rushikesh Pawar <pawarrushi0507@gmail.com>
2022-08-03 11:08:06 +0530

    updated

---
+++ src/day_1/FirstGit.java
@@ -0,0 +1,11 @@
+package day_1;
+
+public class FirstGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My first git project");
+	}
+
+}
